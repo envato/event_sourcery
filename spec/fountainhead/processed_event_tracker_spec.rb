880249bcf4883556bb9ea1231a208bdcc1256b63
Steve Hodgkiss <steve@hodgkiss.me>
2016-04-16 17:06:33 +1000

    Add spec for delegation

---
+++ spec/fountainhead/processed_event_tracker_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe Fountainhead::ProcessedEventTracker do
+  let(:adapter) { double }
+  subject(:processed_event_tracker) { described_class.new(adapter) }
+
+  %w[setup processed_event reset_last_processed_event_id last_processed_event_id tracked_processors ].each do |method|
+    it "delegates #{method} to the adapter" do
+      expect(adapter).to receive(method)
+      processed_event_tracker.send(method)
+    end
+  end
+end

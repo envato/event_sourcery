eb7d48a018deb3f6f888206df93febc1fb72155e
Steve Hodgkiss <steve@hodgkiss.me>
2016-05-13 01:16:48 -0700

    Use a queue to allow a buffer of new event_id's to be filled in each attempt

--- lib/event_sourcery/event_feeder_adapters/postgres_push.rb
+++ lib/event_sourcery/event_feeder_adapters/postgres_push.rb
@@ -6,12 +6,17 @@ module EventSourcery
         @event_source = event_source
         @postgres_subscriber = NewEventSubscriber.new(sequel_connection)
         @listen_args = listen_args
+        @new_event_queue = Queue.new
+        @listen_thread = Thread.new { start_event_listener }
       end
 
       def start!(subscribers)
-        @postgres_subscriber.listen(*@listen_args) do |event_id|
-          subscribers.each do |subscriber|
-            catch_up_subscriber(subscriber, event_id)
+        catch(:stop) do
+          loop do
+            event_id = consume_queue
+            subscribers.each do |subscriber|
+              catch_up_subscriber(subscriber, event_id)
+            end
           end
         end
       end
@@ -22,6 +27,22 @@ module EventSourcery
         @event_source.each_by_range(subscriber.last_seen_event_id + 1, event_id, event_types: subscriber.event_types) do |event|
           subscriber.call(event)
         end
+      end
+
+      def start_event_listener
+        @postgres_subscriber.listen(*@listen_args) do |event_id|
+          @new_event_queue << event_id
+        end
+      end
+
+      def consume_queue
+        throw :stop if !@listen_thread.alive?
+        event_id = @new_event_queue.pop
+        throw :stop if event_id == :stop
+        @new_event_queue.size.times do
+          event_id = @new_event_queue.shift
+        end
+        event_id
       end
     end
   end

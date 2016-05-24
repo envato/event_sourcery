cf20e21574a7e54e5a86cdaf4499bae5a084156a
Steve Hodgkiss <steve@hodgkiss.me>
2016-05-24 12:36:03 -0700

    Remove now unneeded methods

--- lib/event_sourcery/event_sink_adapters/postgres.rb
+++ lib/event_sourcery/event_sink_adapters/postgres.rb
@@ -19,59 +19,6 @@ module EventSourcery
 
       private
 
-      def aggregate_version(aggregate_id)
-        result = aggregates_table.
-          where(aggregate_id: aggregate_id).
-          first
-        if result
-          result[:version]
-        end
-      end
-
-      def resolve_new_aggregate_version(aggregate_id, type, expected_version)
-        current_version = aggregate_version(aggregate_id)
-        if current_version
-          if expected_version.nil?
-            new_version = increment_aggregate_version(aggregate_id)
-          else
-            new_version = current_version + 1
-            update_aggregate_version(aggregate_id, new_version, expected_version)
-          end
-        else
-          insert_aggregate_version(aggregate_id, type, 1, expected_version)
-          new_version = 1
-        end
-        new_version
-      end
-
-      def update_aggregate_version(aggregate_id, version, expected_version)
-        update_query = aggregates_table.
-          where(aggregate_id: aggregate_id)
-        if expected_version
-          update_query = update_query.where(version: expected_version)
-        end
-        if update_query.update(version: version) == 0
-          raise ConcurrencyError, "expected version was not #{expected_version}"
-        end
-      end
-
-      def insert_aggregate_version(aggregate_id, type, version, expected_version)
-        if expected_version && expected_version != 0
-          raise ConcurrencyError, "expected version was #{expected_version}, not 0"
-        end
-        @connection.run <<-SQL
-          insert into aggregates (aggregate_id, type, version)
-            values ('#{aggregate_id}', '#{type}', #{version})
-        SQL
-      end
-
-      def increment_aggregate_version(aggregate_id)
-        aggregates_table.
-          where(aggregate_id: aggregate_id).
-          returning(:version).
-          update(version: Sequel.expr(:version) + 1).first[:version]
-      end
-
       attr_reader :connection
 
       def events_table

ed0c7f6864e55c450206563636be6a1756a60be9
Sebastian von Conrad <sebastian@vonconrad.com>
2016-09-01 21:19:50 +1000

    Setup database as part of project creation
    As an optional step, I think the project creation command could
    also create a dev database (and events table) for the user. It's
    optional, so the user can opt out. We also ask for database config
    to make it even more flexible. All in all, this should help users
    get up and running even faster, as they won't have to worry about
    creating databases.

--- lib/event_sourcery/tasks/create_new_project.rb
+++ lib/event_sourcery/tasks/create_new_project.rb
@@ -17,9 +17,48 @@ module EventSourcery
         template('templates/rakefile.tt', "#{project_name}/Rakefile")
       end
 
+      def create_env_file
+        create_file("#{project_name}/.env")
+      end
+
       def bundle_install
         inside(project_name) do
           run('bundle install', capture: true)
+        end
+      end
+
+      def create_event_store
+        confirmation_text = "Would you like to set up the required Event Store PostgreSQL database? (y/n)"
+
+        if yes?(confirmation_text, :red)
+          db_name = ask('Database name:', default: "#{project_name}_event_store_development")
+          db_user = ask('Database user:', default: 'postgres')
+          db_pass = ask('Database password:', default: '')
+          db_port = ask('Database port:', default: '5432')
+
+          cmd = "PGPASSWORD=#{db_pass} createdb -U #{db_user} -p #{db_port} #{db_name}"
+          run(cmd, capture: true)
+
+          require 'sequel'
+          uri = "postgres://#{db_user}:#{db_pass}@localhost:#{db_port}/#{db_name}"
+          db = Sequel.connect(uri)
+
+          db.create_table :events do
+            primary_key :id, type: :Bignum
+            column :aggregate_id, 'uuid not null'
+            column :type, 'varchar(255) not null'
+            column :body, 'json not null'
+            column :created_at, 'timestamp without time zone not null default (now() at time zone \'utc\')'
+            index :aggregate_id
+            index :type
+            index :created_at
+          end
+
+          append_to_file("#{project_name}/.env") do
+            "EVENT_STORE_DATABASE_URI=#{uri}\n"
+          end
+        else
+          say("Okay. You will need to set up your own Event Store.", :yellow)
         end
       end
     end

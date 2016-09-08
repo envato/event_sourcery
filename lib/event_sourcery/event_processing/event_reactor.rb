a4e6f5821b2d98675cfabb1c715fa0d816cf629c
Steve Hodgkiss <steve@hodgkiss.me>
2016-09-08 14:16:56 +1000

    Remove overriding process method from EventReactor

--- lib/event_sourcery/event_processing/event_reactor.rb
+++ lib/event_sourcery/event_processing/event_reactor.rb
@@ -7,7 +7,6 @@ module EventSourcery
         base.include(EventStreamProcessor)
         base.extend(ClassMethods)
         base.prepend(TableOwner)
-        base.prepend(ProcessHandler)
         base.include(InstanceMethods)
       end
 
@@ -45,24 +44,13 @@ module EventSourcery
 
       DRIVEN_BY_EVENT_PAYLOAD_KEY = :_driven_by_event_id
 
-      module ProcessHandler
-        def process(event)
-          @event = event
-          if self.class.processes?(event.type)
-            super(event)
-          end
-          tracker.processed_event(self.class.processor_name, event.id)
-          @event = nil
-        end
-      end
-
       private
 
-      attr_reader :event_sink, :event_source, :event, :latest_event_id_on_setup
+      attr_reader :event_sink, :event_source
 
       def emit_event(aggregate_id:, type:, body: {}, &block)
         raise UndeclaredEventEmissionError unless self.class.emits_event?(type)
-        body = body.merge(DRIVEN_BY_EVENT_PAYLOAD_KEY => event.id)
+        body = body.merge(DRIVEN_BY_EVENT_PAYLOAD_KEY => _event.id)
         invoke_action_and_emit_event(aggregate_id, type, body, block)
       end
 

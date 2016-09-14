cf87cf1d8509693a52e821ba616f72fc7d03aa96
Matt Ward <mattyjward@gmail.com>
2016-09-14 17:42:16 +1000

    always stringfy hash body for event

--- lib/event_sourcery/event.rb
+++ lib/event_sourcery/event.rb
@@ -2,6 +2,11 @@ module EventSourcery
   class Event
     include Virtus.value_object
 
+    def initialize(**hash)
+      hash[:body] = stringify_keys(hash[:body]) if hash[:body].is_a? Hash
+      super
+    end
+
     values do
       attribute :id, Integer
       attribute :aggregate_id, String
@@ -12,6 +17,15 @@ module EventSourcery
 
     def persisted?
       !id.nil?
+    end
+
+    private
+
+    def stringify_keys(hash)
+      hash.inject({}) do |memo, (key, value)|
+        memo[key.to_s] = value
+        memo
+      end
     end
   end
 end
--- spec/event_sourcery/event_spec.rb
+++ spec/event_sourcery/event_spec.rb
@@ -1,4 +1,40 @@
 RSpec.describe EventSourcery::Event do
+  let(:aggregate_id) { 'aggregate_id' }
+  let(:type) { 'type' }
+  let(:body) do
+    {
+      symbol: "value",
+    }
+  end
+
+  describe '#initialize' do
+    subject(:initializer) { described_class.new(aggregate_id: aggregate_id, type: type, body: body) }
+
+    it 'converts body keys to strings' do
+      expect(initializer.body.keys).to eq ["symbol"]
+    end
+
+    context 'key already a string' do
+      let(:body) do
+        {
+          "string" => "value",
+        }
+      end
+
+      it 'keeps string keys in body as strings' do
+        expect(initializer.body.keys).to eq ["string"]
+      end
+    end
+
+    context 'body is nil' do
+      let(:body) { nil }
+
+      it 'body is nil' do
+        expect(initializer.body).to be_nil
+      end
+    end
+  end
+
   context 'equality' do
     #let(:event_1) { EventSourcery::Event.new(id: 1
   end

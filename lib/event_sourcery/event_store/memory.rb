9342d13fdf80c3872c23b4a0a6ec2a18b566fa4e
Andrei Gridnev <andrew.gridnev@gmail.com>
2016-11-28 22:54:44 +1100

    Simplify code in in-memory event store

--- lib/event_sourcery/event_store/memory.rb
+++ lib/event_sourcery/event_store/memory.rb
@@ -33,26 +33,14 @@ module EventSourcery
       end
 
       def latest_event_id(event_types: nil)
-        event = if event_types
-                  @events.
-                    select { |e| event_types.include?(e.type) }.
-                    last
-                else
-                  @events.last
-                end
-        if event
-          event.id
-        else
-          0
-        end
+        event = event_types ? @events.select { |e| event_types.include?(e.type) }.last : @events.last
+
+        event ? event.id : 0
       end
 
       def get_events_for_aggregate_id(id)
-        @events.select { |event|
-          event.aggregate_id == id
-        }
+        @events.select { |event| event.aggregate_id == id }
       end
     end
   end
 end
-

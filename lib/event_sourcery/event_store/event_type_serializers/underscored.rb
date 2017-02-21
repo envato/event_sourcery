5ecb2fc3cc4afc429f457dff0637e749a1327032
Steve Hodgkiss <steve@hodgkiss.me>
2017-02-21 16:03:39 +1100

    Avoid dependency on active support, copy over the required inflector methods

--- lib/event_sourcery/event_store/event_type_serializers/underscored.rb
+++ lib/event_sourcery/event_store/event_type_serializers/underscored.rb
@@ -1,5 +1,3 @@
-require 'active_support/all' # TODO: only load string exts and consider alternatives to this dependency
-
 module EventSourcery
   module EventStore
     module EventTypeSerializers
@@ -24,14 +22,47 @@ module EventSourcery
           end
         end
 
+        private
+
         def lookup_type(event_type)
-          Object.const_get(event_type.camelize)
+          Object.const_get(camelize(event_type))
         rescue NameError
           Event
         end
 
         def underscore_class_name(class_name)
-          class_name.underscore
+          underscore(class_name)
+        end
+
+        # Inflection methods are taken from active support 3.2
+        # https://github.com/rails/rails/blob/3-2-stable/activesupport/lib/active_support/inflector/methods.rb
+        def underscore(camel_cased_word)
+          word = camel_cased_word.to_s.dup
+          word.gsub!(/::/, '/')
+          word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
+          word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+          word.tr!("-", "_")
+          word.downcase!
+          word
+        end
+
+        def camelize(term, uppercase_first_letter = true)
+          string = term.to_s
+          if uppercase_first_letter
+            string = string.sub(/^[a-z\d]*/) { capitalize($&) }
+          else
+            string = string.sub(/^(?:(?=\b|[A-Z_])|\w)/) { $&.downcase }
+          end
+          string.gsub(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{capitalize($2)}" }.gsub('/', '::')
+        end
+
+        def capitalize(lower_case_and_underscored_word)
+          result = lower_case_and_underscored_word.to_s.dup
+          result.gsub!(/_id$/, "")
+          result.gsub!(/_/, ' ')
+          result.gsub(/([a-z\d]*)/i) { |match|
+            "#{match.downcase}"
+          }.gsub(/^\w/) { $&.upcase }
         end
       end
     end

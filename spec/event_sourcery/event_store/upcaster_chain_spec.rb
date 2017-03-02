013c8d2dea48f0a4a4739b167878530d3cbd8a3d
Steve Hodgkiss <steve@hodgkiss.me>
2017-03-02 17:53:14 +1100

    Spec the no upcaster defined case

--- spec/event_sourcery/event_store/upcaster_chain_spec.rb
+++ spec/event_sourcery/event_store/upcaster_chain_spec.rb
@@ -1,33 +1,42 @@
 RSpec.describe EventSourcery::EventStore::UpcasterChain do
   subject(:upcaster_chain) { described_class.new }
 
-  before do
-    upcaster_chain.define('ItemAdded', 'default currency is USD') do |body|
-      body['currency'] ||= 'USD'
-    end
-  end
-
-  it 'applies transformations for a given class' do
-    upcasted_body = upcaster_chain.upcast('ItemAdded', { 'name' => 'Bug' })
-    expect(upcasted_body).to eq('name' => 'Bug', 'currency' => 'USD')
-  end
-
-  it 'does nothing if none are defined' do
-    upcasted_body = upcaster_chain.upcast('ItemRemoved', { 'name' => 'Bug' })
-    expect(upcasted_body).to eq('name' => 'Bug')
-  end
-
-  context 'with more than one upcaster function' do
+  context 'when there are upcasters defined' do
     before do
-      upcaster_chain.define('ItemAdded') do |body|
-        body['currency'] ||= 'AUD'
-        body['upcaster_2'] = 'check'
+      upcaster_chain.define('ItemAdded', 'default currency is USD') do |body|
+        body['currency'] ||= 'USD'
       end
     end
 
-    it 'runs them in order' do
+    it 'applies transformations for a given class' do
       upcasted_body = upcaster_chain.upcast('ItemAdded', { 'name' => 'Bug' })
-      expect(upcasted_body).to eq('name' => 'Bug', 'currency' => 'USD', 'upcaster_2' => 'check')
+      expect(upcasted_body).to eq('name' => 'Bug', 'currency' => 'USD')
+    end
+
+    it 'does nothing if none are defined' do
+      upcasted_body = upcaster_chain.upcast('ItemRemoved', { 'name' => 'Bug' })
+      expect(upcasted_body).to eq('name' => 'Bug')
+    end
+
+    context 'with more than one upcaster function' do
+      before do
+        upcaster_chain.define('ItemAdded') do |body|
+          body['currency'] ||= 'AUD'
+          body['upcaster_2'] = 'check'
+        end
+      end
+
+      it 'runs them in order' do
+        upcasted_body = upcaster_chain.upcast('ItemAdded', { 'name' => 'Bug' })
+        expect(upcasted_body).to eq('name' => 'Bug', 'currency' => 'USD', 'upcaster_2' => 'check')
+      end
+    end
+  end
+
+  context 'when there are no upcasters for a given type' do
+    it 'returns the body as is' do
+      upcasted_body = upcaster_chain.upcast('ItemAdded', { 'name' => 'Bug' })
+      expect(upcasted_body).to eq('name' => 'Bug')
     end
   end
 end

5aa2faf7fd5fbaee14dca38cf661e3efedb52b9b
Steve Hodgkiss <steve@hodgkiss.me>
2017-05-13 09:16:26 +1000

    Extract optimistic locking specs to shared examples

--- spec/support/event_store_shared_examples.rb
+++ spec/support/event_store_shared_examples.rb
@@ -37,9 +37,7 @@ RSpec.shared_examples 'an event store' do
       expect(events.count).to eq 3
       expect(events.map(&:id)).to eq [1, 2, 3]
       expect(events.map(&:body)).to eq [{'e' => 1}, {'e' => 2}, {'e' => 3}]
-      if supports_versions
-        expect(events.map(&:version)).to eq [1, 2, 3]
-      end
+      expect(events.map(&:version)).to eq [1, 2, 3]
     end
 
     it 'sets the correct aggregates version' do
@@ -85,6 +83,20 @@ RSpec.shared_examples 'an event store' do
         expect(events.last.type).to eq :test_event_2.to_s # shouldn't you get back what you put in, a symbol?
         expect(events.last.body).to eq({ 'my' => 'data' }) # should we symbolize keys when hydrating events?
       end
+    end
+
+    it 'correctly inserts created at times when inserting multiple events atomically' do
+      time = Time.parse('2016-10-14T00:00:00.646191Z')
+      event_store.sink([new_event(aggregate_id: aggregate_id, created_at: nil), new_event(aggregate_id: aggregate_id, created_at: time)])
+      created_ats = event_store.get_next_from(0).map(&:created_at)
+      expect(created_ats.map(&:class)).to eq [Time, Time]
+      expect(created_ats.last).to eq time
+    end
+
+    it 'raises an error if the events given are for more than one aggregate' do
+      expect {
+        event_store.sink([new_event(aggregate_id: aggregate_id), new_event(aggregate_id: SecureRandom.uuid)])
+      }.to raise_error(EventSourcery::AtomicWriteToMultipleAggregatesNotSupported)
     end
   end
 
@@ -204,6 +216,114 @@ RSpec.shared_examples 'an event store' do
         expect(events_by_range(1, 21, event_types: ['user_signed_up']).count).to eq 0
         expect(events_by_range(1, 21, event_types: ['item_added']).count).to eq 21
       end
+    end
+  end
+
+  def save_event(expected_version: nil)
+    event_store.sink(new_event(aggregate_id: aggregate_id,
+                     type: :billing_details_provided,
+                     body: { my_event: 'data' }),
+                     expected_version: expected_version)
+  end
+
+  def add_event
+    event_store.sink(new_event(aggregate_id: aggregate_id))
+  end
+
+  def last_event
+    event_store.get_next_from(0).last
+  end
+
+  def aggregate_version
+    result = connection[:aggregates].
+      where(aggregate_id: aggregate_id).
+      first
+    if result
+      result[:version]
+    end
+  end
+
+  context 'optimistic concurrency control' do
+    context "when the aggregate doesn't exist" do
+      context 'and the expected version is correct - 0' do
+        it 'saves the event with and sets the aggregate version to version 1' do
+          save_event(expected_version: 0)
+          expect(last_event.version).to eq 1
+          expect(aggregate_version).to eq 1
+        end
+      end
+
+      context 'and the expected version is incorrect - 1' do
+        it 'raises a ConcurrencyError' do
+          expect {
+            save_event(expected_version: 1)
+          }.to raise_error(EventSourcery::ConcurrencyError)
+        end
+      end
+
+      context 'with no expected version' do
+        it 'saves the event with and sets the aggregate version to version 1' do
+          save_event
+          expect(last_event.version).to eq 1
+          expect(aggregate_version).to eq 1
+        end
+      end
+    end
+
+    context 'when the aggregate exists' do
+      before do
+        add_event
+      end
+
+      context 'with an incorrect expected version - 0' do
+        it 'raises a ConcurrencyError' do
+          expect {
+            save_event(expected_version: 0)
+          }.to raise_error(EventSourcery::ConcurrencyError)
+        end
+      end
+
+      context 'with a correct expected version - 1' do
+        it 'saves the event with and sets the aggregate version to version 2' do
+          save_event
+          expect(last_event.version).to eq 2
+          expect(aggregate_version).to eq 2
+        end
+      end
+
+      context 'with no aggregate version' do
+        it 'automatically sets the version on the event and aggregate' do
+          save_event
+          expect(last_event.version).to eq 2
+          expect(aggregate_version).to eq 2
+        end
+      end
+    end
+
+    context 'when a database error occurs that is not a concurrency error' do
+      before do
+        allow(connection).to receive(:run).and_raise(Sequel::DatabaseError)
+      end
+
+      it 'raises it' do
+        expect { add_event }.to raise_error(Sequel::DatabaseError)
+      end
+    end
+
+    it 'allows overriding the created_at timestamp for events' do
+      time = Time.parse('2016-10-14T00:00:00.646191Z')
+      event_store.sink(new_event(aggregate_id: aggregate_id,
+                                 type: :billing_details_provided,
+                                 body: { my_event: 'data' },
+                                 created_at: time))
+      expect(last_event.created_at).to eq time
+    end
+
+    it 'defaults to now() when no created_at timestamp is supplied' do
+      event_store.sink(new_event(aggregate_id: aggregate_id,
+                                 type: :billing_details_provided,
+                                 body: { my_event: 'data' }))
+      expect(last_event.created_at).to be_instance_of(Time)
     end
   end
 end

64a6ff7fa7ea5fb03c3c1caa04a9b29d9f9f3b4f
Jiexin Huang <jiexin.huang@envato.com>
2017-05-23 09:41:28 +1000

    move update interval logic into update_retry_interval method

--- lib/event_sourcery/event_processing/error_handlers/exponential_backoff_retry.rb
+++ lib/event_sourcery/event_processing/error_handlers/exponential_backoff_retry.rb
@@ -15,13 +15,7 @@ module EventSourcery
           yield
         rescue => error
           report_error(error)
-
-          if error.instance_of?(EventSourcery::EventProcessingError)
-            update_retry_interval(error)
-          else
-            @retry_interval = DEFAULT_RETRY_INVERAL
-          end
-
+          update_retry_interval(error)
           sleep(@retry_interval)
           retry
         end
@@ -29,10 +23,14 @@ module EventSourcery
         private
 
         def update_retry_interval(error)
-          if @error_event_uuid == error.event.uuid
-            @retry_interval *=2 if @retry_interval < MAX_RETRY_INVERVAL
+          if error.instance_of?(EventSourcery::EventProcessingError)
+            if @error_event_uuid == error.event.uuid
+              @retry_interval *=2 if @retry_interval < MAX_RETRY_INVERVAL
+            else
+              @error_event_uuid = error.event.uuid
+              @retry_interval = DEFAULT_RETRY_INVERAL
+            end
           else
-            @error_event_uuid = error.event.uuid
             @retry_interval = DEFAULT_RETRY_INVERAL
           end
         end

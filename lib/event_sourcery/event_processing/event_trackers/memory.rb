18f4c20f458e86b1ab5e75a772f49a61350d4397
Joe Sustaric <joe.sustaric@envato.com>
2017-06-14 11:08:15 +1000

    moarrrr docoooo

--- lib/event_sourcery/event_processing/event_trackers/memory.rb
+++ lib/event_sourcery/event_processing/event_trackers/memory.rb
@@ -1,15 +1,27 @@
 module EventSourcery
   module EventProcessing
     module EventTrackers
+      # Being able to know where you're at when reading an event stream
+      # is important. In here are mechanisms to do so.
       class Memory
+        # Tracking where you're at via a simple in memory hash.
+
         def initialize
           @state = Hash.new(0)
         end
 
+        # Register a new processor to track.
+        # Will start from 0.
+        #
+        # @param processor_name [String] the name of the processor to track
         def setup(processor_name)
           @state[processor_name] = 0
         end
 
+        # Update the given processor name to the given event id number.
+        #
+        # @param processor_name [String] the name of the processor to update
+        # @param event_it [Int] the number of the event to update
         def processed_event(processor_name, event_id)
           @state[processor_name.to_s] = event_id
         end
@@ -23,10 +35,16 @@ module EventSourcery
           @state[processor_name.to_s] = 0
         end
 
+        # Find the last processed event id for a given processor name.
+        #
+        # @return [Int] the last event id that the given processor has processed
         def last_processed_event_id(processor_name)
           @state.fetch(processor_name.to_s, 0)
         end
 
+        # Returns an array of all the processors that are being tracked.
+        #
+        # @return [Array] an array of names of the tracked processors
         def tracked_processors
           @state.keys
         end

3f379f2ce5b660ca0ab88ecc17f65b8506a57791
Andrei Gridnev <andrew.gridnev@gmail.com>
2017-06-16 10:25:49 +1000

    Stringify aggregate_id when loading aggregate from event_store

--- lib/event_sourcery/rspec/event_store_shared_examples.rb
+++ lib/event_sourcery/rspec/event_store_shared_examples.rb
@@ -176,16 +176,34 @@ RSpec.shared_examples 'an event store' do
   end
 
   describe '#get_events_for_aggregate_id' do
-    it 'gets events for a specific aggregate id' do
-      event_store.sink(new_event(aggregate_id: aggregate_id, type: 'item_added', body: { 'my' => 'body' }))
-      event_store.sink(new_event(aggregate_id: aggregate_id))
-      event_store.sink(new_event(aggregate_id: SecureRandom.uuid))
-      events = event_store.get_events_for_aggregate_id(aggregate_id)
-      expect(events.map(&:id)).to eq([1, 2])
-      expect(events.first.aggregate_id).to eq aggregate_id
-      expect(events.first.type).to eq 'item_added'
-      expect(events.first.body).to eq({ 'my' => 'body' })
-      expect(events.first.created_at).to be_instance_of(Time)
+    RSpec.shared_examples 'gets events for a specific aggregate id' do
+      before do
+        event_store.sink(new_event(aggregate_id: aggregate_id, type: 'item_added', body: { 'my' => 'body' }))
+        event_store.sink(new_event(aggregate_id: aggregate_id))
+        event_store.sink(new_event(aggregate_id: SecureRandom.uuid))
+      end
+
+      subject(:events) { event_store.get_events_for_aggregate_id(uuid) }
+
+      specify do
+        expect(events.map(&:id)).to eq([1, 2])
+        expect(events.first.aggregate_id).to eq aggregate_id
+        expect(events.first.type).to eq 'item_added'
+        expect(events.first.body).to eq({ 'my' => 'body' })
+        expect(events.first.created_at).to be_instance_of(Time)
+      end
+    end
+
+    context 'when aggregate_id is a string' do
+      include_examples 'gets events for a specific aggregate id' do
+        let(:uuid) { aggregate_id }
+      end
+    end
+
+    context 'when aggregate_id is convertible to a string' do
+      include_examples 'gets events for a specific aggregate id' do
+        let(:uuid) { double(to_str: aggregate_id) }
+      end
     end
   end
 

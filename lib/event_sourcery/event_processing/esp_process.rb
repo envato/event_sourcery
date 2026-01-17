4eff266312105e0f4e216493b13e661ceca6fb25
Orien Madgwick <vcs@orien.io>
2026-01-17 19:43:01 +0700

    RuboCop: disable `Lint/RescueException` - it is intended

--- lib/event_sourcery/event_processing/esp_process.rb
+++ lib/event_sourcery/event_processing/esp_process.rb
@@ -25,7 +25,7 @@ module EventSourcery
           subscribe_to_event_stream
           EventSourcery.logger.info("Stopping #{processor_name}")
         end
-      rescue Exception => e
+      rescue Exception => e # rubocop:disable Lint/RescueException
         EventSourcery.logger.fatal("An unhandled exception occurred in #{processor_name}")
         EventSourcery.logger.fatal(e)
         EventSourcery.config.on_event_processor_critical_error.call(e, processor_name)
